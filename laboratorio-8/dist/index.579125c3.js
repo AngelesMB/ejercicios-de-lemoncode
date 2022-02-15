b9e32de126ecab998f163a765f9ab0dcb4654a1a
AngelesMB <angeles.martinezbarrio@gmail.com>
2022-02-15 17:28:00 +0100

    Bring repos together, rename folders

--- laboratorio-8/dist/index.579125c3.js
+++
@@ -1,9 +0,0 @@
-// ES6
-// import * as ClientListBusiness from "./client-list-business";
-// COMMON JS
-// const ClientListBusiness = require("./client-list-business");
-window.onload = function() {
-    ClientListBusiness.printClientsAccounts();
-};
-
-//# sourceMappingURL=index.579125c3.js.map
